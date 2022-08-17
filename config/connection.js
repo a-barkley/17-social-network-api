7bebd6e55533768e5ae4d8da887484caff564fa5
pbarkley_desk <rpbarkley99@gmail.com>
2022-08-16 20:21:39 -0500

    finish server.js

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+module.exports = mongoose.connection
